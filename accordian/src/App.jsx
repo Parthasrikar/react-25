f0138b798a023e950357cf692722a456c8d9825e
Partha <parthasrikar853@gmail.com>
2024-10-09 12:03:51 +0530

    showing all projects

--- accordian/src/App.jsx
+++ accordian/src/App.jsx
@@ -6,9 +6,9 @@ import { ImageSlider } from "./components/Image-slider/ImageSlider";
 function App() {
 
   return <div>
-      {/* <Accord></Accord> */}
-      {/* <Rcolor></Rcolor> */}
-      {/* <StarRating noOfStars={10}></StarRating> */}
+      <Accord></Accord>
+      <Rcolor></Rcolor>
+      <StarRating noOfStars={10}></StarRating>
       <ImageSlider url={'https://picsum.photos/v2/list'} limit={"10"}></ImageSlider>
     </div>
 }
